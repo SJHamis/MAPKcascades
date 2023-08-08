85a612fb694bf20257adb1e3c2e1641991b1cf98
Alice De Carli <alice.de.carli01@gmail.com>
2023-08-08 14:05:19 +0100

    update the Sact_surface.m file to include synergy surfaces in IC50 three drug combinations surface plots

--- extended_code/three_drugs/Sact_surface.m
+++ extended_code/three_drugs/Sact_surface.m
@@ -10,13 +10,13 @@ plot_location = strcat(pwd,'/auxiliary_files_plots');
 addpath(plot_location);
 
 DBF_min=0;
-DBF_max=5;%10;
+DBF_max=5;
 DBF_n=50;
 DBF_h=(DBF_max-DBF_min)/DBF_n;
 DBF_range=DBF_min:DBF_h:DBF_max;
 
 TMT_min=0;
-TMT_max=5;%2.5;
+TMT_max=5;
 TMT_n=50;
 TMT_h=(TMT_max-TMT_min)/TMT_n;
 TMT_range=TMT_min:TMT_h:TMT_max;
@@ -31,7 +31,7 @@ SCH_concentration=zeros(length(TMT_range),length(DBF_range));
 
 userinput_BRAF_tot = 0.003; % change to investigate other BRAF concentrations
 userinput_ATP_tot = 1000; % change to investigate other ATP concentrations
-tend=24;%in hours
+tend=24; % in hours
 
 set(gcf,'position',[100,100,1200,700])
     for i_SCH = 1:length(SCH_range)
@@ -49,15 +49,46 @@ set(gcf,'position',[100,100,1200,700])
                 end
             end
     end
-        
-        surf(TMT_range, DBF_range, SCH_concentration);
-        grid('on')
-        xlabel('TMT (\muM)','FontSize',14)
-        xlim([0 5])
-        ylabel('DBF (\muM)' ,'FontSize',14)
-        ylim([0 5])
-        set(gca, 'YDir','reverse')
-        zlabel('SCH (\muM)', 'FontSize',14)
-        zlim([0 5])
+
+surf(TMT_range, DBF_range, SCH_concentration);
+a = get(gca,'Clim');
+
+hold on
+
+% plane eqn through 3 monotherapy points calculated separately
+
+% A = 2.89 for BRAF = 3 nM, ATP = 1 mM;
+%   = 10.2 for BRAF = 10 nM, ATP = 1 mM;
+%   = 37.45 for BRAF = 3 nM, ATP = 5 mM.
+A = 2.89;
+
+% B = 4.93 for BRAF = 3 nM, ATP = 1 mM;
+%   = 4.93 for BRAF = 10 nM, ATP = 5 mM;
+%   = 18.2 for BRAF = 3 nM, ATP = 1 mM.
+B = 4.93;
+
+% C = 4.93 for BRAF = 3 nM, ATP = 1 mM;
+%   = 17.4 for BRAF = 10 nM, ATP = 1 mM;
+%   = 55.64 for BRAF = 3 nM, ATP = 5 mM.
+C = 4.93;
+
+% D = -8.381 for BRAF = 3 nM, ATP = 1 mM;
+%   = -29.58 for BRAF = 10 nM, ATP = 1 mM;
+%   = -194.74 for BRAF = 3 nM, ATP = 5 mM.
+D = -8.381;
+
+[X,Y] = meshgrid(TMT_range,DBF_range);
+synergy_plane = (-1/C)*(A*X + B*Y + D); % synergy plane
+surf(TMT_range, DBF_range, synergy_plane, ...
+    'FaceColor','red', 'FaceAlpha',0.5, 'EdgeColor','none');
+
+axis([0 5 0 5 0 5])
+set(gca,'Clim',a);
+
+grid on
+xlabel('TMT (\muM)','FontSize',14)
+ylabel('DBF (\muM)' ,'FontSize',14)
+set(gca, 'YDir','reverse')
+zlabel('SCH (\muM)', 'FontSize',14)
 
 title({'IC_{50} : BRAF = 3 nM, ATP = 1 mM'}, 'FontSize', 14)
